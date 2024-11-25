998158022dc6fd33741a7abbf45284f8f5cf2026
wicne <63619960+wicne@users.noreply.github.com>
2024-11-25 14:30:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,41 +2,24 @@ from flask import Flask, request, jsonify
 import pickle
 import pandas as pd
 
-# Flask uygulamasını başlat
 app = Flask(__name__)
 
 # Modeli yükle
-try:
-    model = pickle.load(open('model.pkl', 'rb'))
-except Exception as e:
-    print(f"Model yüklenemedi: {e}")
-    model = None
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/', methods=['GET'])
 def home():
-    return "Flask API Çalışıyor! Model tahmin için hazır."
+    return "Flask API Çalışıyor!"
 
-# Tahmin yapmak için bir endpoint
 @app.route('/predict', methods=['POST'])
 def predict():
-    if not model:
-        return jsonify({'error': 'Model yüklenemedi veya mevcut değil'}), 500
-
-    # Kullanıcıdan gelen JSON verisini al
     data = request.get_json(force=True)
     try:
-        # JSON verisinin 'features' anahtarını doğrula
-        if 'features' not in data:
-            return jsonify({'error': "Gönderilen JSON'da 'features' anahtarı eksik."}), 400
-
-        # Gelen veriyi işleme
         features = pd.DataFrame([data['features']], columns=["team1", "team2", "weather", "time"])
-
-        # Tahmin yap
         prediction = model.predict(features)[0]
         return jsonify({'prediction': prediction})
     except Exception as e:
-        return jsonify({'error': f"Veri işleme veya tahmin hatası: {str(e)}"}), 400
+        return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
